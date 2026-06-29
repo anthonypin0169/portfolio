bc6de2e47e8ff4812b24c92bfc9a5cbeafc05471
Anthony Pin <anthonypin0169@gmail.com>
2026-06-29 17:35:15 +0200

    Ajout du style du carrousel

--- src/data/schoolProjects.js
+++ src/data/schoolProjects.js
@@ -2,25 +2,25 @@ const schoolProjects = [
     {
         id: 1,
         title: "Kasa",
-        picture: ["../assets/images/Kasa1.png","../assets/images/Kasa2.png","../assets/images/Kasa3.png"],
+        picture: ["./images/Kasa1.png","./images/Kasa2.png","./images/Kasa3.png"],
         alt:"Photo du projet Kasa",
-        logoTech:[".../assets/images/ReactLogo.png"],
+        logoTech:["./images/ReactLogo.png"],
         repository:"https://github.com/anthonypin0169/OpenClassroomsProject-7"
     },
     {
         id: 2,
         title: "ArgentBank",
-        picture: ["../assets/images/ArgentBank1.png","../assets/images/ArgentBank2.png","../assets/images/ArgentBank3.png"],
+        picture: ["./images/ArgentBank1.png","./images/ArgentBank2.png","./images/ArgentBank3.png"],
         alt:"Photo du projet ArgentBank",
-        logoTech:["../assets/images/ReactLogo.png","../assets/images/ReduxLogo.png"],
+        logoTech:["./images/ReactLogo.png","./images/ReduxLogo.png"],
         repository:"https://github.com/anthonypin0169/OpenClassroomsProject10"
     },
     {
         id: 3,
-        title: "Menu-Maker / Qwenta",
-        picture: ["../assets/images/MenuMaker1.png","../assets/images/MenuMaker2.png","../assets/images/MenuMaker3.png"],
+        title: "Menu-Maker/Qwenta",
+        picture: ["./images/MenuMaker1.png","./images/MenuMaker2.png","./images/MenuMaker3.png"],
         alt:"Photo du projet Menu-Maker / Qwenta",
-        logoTech:["../assets/images/NotionLogo.png","../assets/images/FeedlyLogo.png"],
+        logoTech:["./images/NotionLogo.png","./images/FeedlyLogo.png"],
         repository:"https://difficult-toucan-1f4.notion.site/Kanban-Qwenta-36f9202c3e0880be9365d81c60ef440a"
     }
 ]
--- src/pages/carrousel.jsx
+++ src/pages/carrousel.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import schoolProjects from "../data/schoolProjects"
+import "./carrousel.scss"
 function Carrousel () {
 
     const [carrouselState, setCarrouselState] = useState(0)
@@ -18,12 +19,12 @@ function Carrousel () {
     }
 
     return(
-        <div className="carrousel-card">
+        <div className="carrousel">
             <article className="carrousel__card">
                 <h2 className="carrousel__card--title">{schoolProjects[carrouselState].title}</h2>
-                <img src={schoolProjects[carrouselState].picture} alt={schoolProjects[carrouselState].alt} className="carrousel__card--img" />
-                <button onClick={handlePrev} className="carrousel__card--arrow fa-angle-left"></button>
-                <button onClick={handleNext} className="carrousel__card--arrow fa-angle-right"></button>
+                <img src={schoolProjects[carrouselState].picture[0]} alt={schoolProjects[carrouselState].alt} className="carrousel__card--img" />
+                <button onClick={handlePrev} className="carrousel__card--arrow left-btn"><i className="fa-solid fa-angle-left"></i></button>
+                <button onClick={handleNext} className="carrousel__card--arrow right-btn"><i className="fa-solid fa-angle-right"></i></button>
             </article>
         </div>
     )
