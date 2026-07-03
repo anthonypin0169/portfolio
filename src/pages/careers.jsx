80eee3232d2a012177e222ad87210f1fa39c6d2b
Anthony Pin <anthonypin0169@gmail.com>
2026-07-03 18:26:17 +0200

    ajout de l'animation ccs-anim

--- src/pages/careers.jsx
+++ src/pages/careers.jsx
@@ -1,4 +1,56 @@
+import { useState } from "react"
+import "./careers.scss"
 function Careers () {
+   
+    const [rotate, setRotate] = useState([false, false, false])
 
+    const toggleCard = (index) => {
+        setRotate(rotate.map((val, i) => i === index ? !val : val))
+    }
+
+
+    return(
+        <main>
+
+            <h1 className="title">Mes compétances</h1>
+
+            <section className="css-anim">
+
+                <h2 className="css-anim__h2">Animations CSS</h2>
+                <div className="css-anim__card-group">
+                    <div className={`card ${rotate[0] ? "flipped" : "" }`} onClick={() => toggleCard(0)}>
+                        <div className="card__front">
+                            <div className={`card__front--content ${rotate[0] ? "shine" : ""}`}>C</div>
+                        </div>
+                        <div className="card__back">
+                            <div className="card__back--content"></div>
+                        </div>
+                    </div>
+
+                    <div className={`card ${rotate[1] ? "flipped" : "" }`} onClick={() => toggleCard(1)}>
+                        <div className="card__front">
+                            <div className={`card__front--content ${rotate[1] ? "shine" : ""}`}>S</div>
+                        </div>
+                        <div className="card__back">
+                            <div className="card__back--content"></div>
+                        </div>
+                    </div>
+
+                    <div className={`card ${rotate[2] ? "flipped" : "" }`} onClick={() => toggleCard(2)}>
+                        <div className="card__front">
+                            <div className={`card__front--content ${rotate[2] ? "shine" : ""}`}>S</div>
+                        </div>
+                        <div className="card__back">
+                            <div className="card__back--content"></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="css-responsive">
+
+            </section>
+        </main>
+    )
 }
 export default Careers
