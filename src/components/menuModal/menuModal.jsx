1164c80a40886a724f985476c34dea464ea67207
Anthony Pin <anthonypin0169@gmail.com>
2026-07-09 08:51:24 +0200

    push pour verification

--- src/components/menuModal/menuModal.jsx
+++ src/components/menuModal/menuModal.jsx
@@ -11,6 +11,7 @@ function MenuModal ({isOpen, onClose}) {
                         <Nav text = "Hobbies tech et ressources" redirection = "/projets" className = {`link link-grp1 ${isOpen ? 'slide' : ''}`}/>
                         <Nav text = "Certifications et témoignages" redirection = "/#certifications" className = {`link link-grp1 ${isOpen ? 'slide' : ''}`}/>
                         <Nav text = "Á propos" redirection = "/#a-propos" className = {`link link-grp1 ${isOpen ? 'slide' : ''}`}/>
+                        <Nav text = "Parcours" redirection = "/#parcours" className = {`link link-grp1 ${isOpen ? 'slide' : ''}`}/>
                     </div>  
 
                     <div className="header__menuModal--group-2">
