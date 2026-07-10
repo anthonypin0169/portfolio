bec647852d3ac3eb97329f4df0c9d39ebae74f88
Anthony Pin <anthonypin0169@gmail.com>
2026-07-10 18:34:32 +0200

    Ajout du responsive de home et du toggle menuModal - footer. Reste à intégrer le toggle Carrousel - card

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,4 +1,24 @@
-function Footer () {
+import Nav from "../nav/nav"
+import "./footer.scss"
 
+function Footer () {
+    return(
+        <footer className="footer">
+            <div className="footer__group group-1">
+                <Nav text = "Hobbies tech et ressources" redirection = "/projets" className = "footer__group--link"/>
+                <Nav text = "Certifications et témoignages" redirection = "/#certifications" className = "footer__group--link"/>
+            </div>  
+
+            <div className="footer__group group-2">
+                <Nav text = "À propos" redirection = "/#a-propos" className = "footer__group--link"/>
+                <Nav text = "Parcours" redirection = "/#parcours" className = "footer__group--link"/>
+            </div>
+            
+            <div className="footer__group group-3">
+                <Nav text = "Mentions légales" redirection = "/mentions-legales" className = "footer__group--link" />
+                <p className = "footer__group--link" >Tous droits réservés</p>
+            </div>
+        </footer>
+    )
 }
 export default Footer
